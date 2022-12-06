2659539e2a5c2b4dabbdf27a6d65a4c50cbbc184
Tom-Ferr <ferr.wav@gmail.com>
2022-12-06 20:26:23 -0300

    making it more readable

--- data_analysis.py
+++ data_analysis.py
@@ -4,6 +4,61 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import sys
 
+def split_in_groups(data):
+    groups = data.dropna().groupby(1)
+
+    M = groups.get_group('M').drop(1, axis=1, inplace=False)
+    M.rename(columns={0: 1}, inplace=True)
+
+    B = groups.get_group('B').drop(1, axis=1, inplace=False)
+    B.rename(columns={0: 1}, inplace=True)
+    return M,B
+
+def plot_histogram(M,B):
+    fig, ax = plt.subplots(4,8)
+    i = j = 0
+    for col in range(1,32):
+        ax[i][j].hist(M[col], bins=15, edgecolor='black',color='red', alpha=0.3)
+        ax[i][j].hist(B[col], bins=15, edgecolor='black',color='cyan', alpha=0.3)
+        i = (i+1)%4 
+        if i == 0:
+            j += 1
+    fig.delaxes(ax[3,7])
+    plt.show()
+
+def plot_scatter(M,B):
+    fig, ax = plt.subplots(4,8)
+    i = j = 0
+    for col in range(1,32):
+        ax[i][j].scatter(M.index, M[col], edgecolor='black',color='red', alpha=0.3)
+        ax[i][j].scatter(B.index, B[col], edgecolor='black',color='cyan', alpha=0.3)
+        i = (i+1)%4 
+        if i == 0:
+            j += 1
+    fig.delaxes(ax[3,7])
+    plt.show()
+
+def plot_matrix(M,B):
+    fig, ax = plt.subplots(4,8)
+    i = j = 0
+    try:
+        if len(sys.argv) >= 3:
+            k = int(sys.argv[2])
+        else:
+            k = 1
+        for col in range(1,32):
+            ax[i][j].scatter(M[k], M[col], edgecolor='black',color='red', alpha=0.3)
+            ax[i][j].scatter(B[k], B[col], edgecolor='black',color='cyan', alpha=0.3)
+            i = (i+1)%4 
+            if i == 0:
+                j += 1
+    except:
+        print("Please, the 3rd argument must be an integer from 1 to 31")
+        exit(3)
+    fig.delaxes(ax[3,7])
+    plt.show()
+
+
 
 """
 MAIN
@@ -32,55 +87,14 @@ if __name__ ==  "__main__":
         exit(2)
     try:
 
-        groups = data.dropna().groupby(1)
-
-        M = groups.get_group('M').drop(1, axis=1, inplace=False)
-        M.rename(columns={0: 1}, inplace=True)
-
-        B = groups.get_group('B').drop(1, axis=1, inplace=False)
-        B.rename(columns={0: 1}, inplace=True)
+        M,B = split_in_groups(data)
 
         if (sys.argv[1] == '--histogram' or sys.argv[1] == '-h'):
-            fig, ax = plt.subplots(4,8)
-            i = j = 0
-            for col in range(1,32):
-                ax[i][j].hist(M[col], bins=15, edgecolor='black',color='red', alpha=0.3)
-                ax[i][j].hist(B[col], bins=15, edgecolor='black',color='cyan', alpha=0.3)
-                i = (i+1)%4 
-                if i == 0:
-                    j += 1
-            fig.delaxes(ax[3,7])
-            plt.show()
+            plot_histogram(M,B)
         elif (sys.argv[1] == '--scatter' or sys.argv[1] == '-s'):
-            fig, ax = plt.subplots(4,8)
-            i = j = 0
-            for col in range(1,32):
-                ax[i][j].scatter(M.index, M[col], edgecolor='black',color='red', alpha=0.3)
-                ax[i][j].scatter(B.index, B[col], edgecolor='black',color='cyan', alpha=0.3)
-                i = (i+1)%4 
-                if i == 0:
-                    j += 1
-            fig.delaxes(ax[3,7])
-            plt.show()
+            plot_scatter(M,B)
         elif (sys.argv[1] == '--matrix' or sys.argv[1] == '-m'):
-            fig, ax = plt.subplots(4,8)
-            i = j = 0
-            try:
-                if len(sys.argv) >= 3:
-                    k = int(sys.argv[2])
-                else:
-                    k = 1
-                for col in range(1,32):
-                    ax[i][j].scatter(M[k], M[col], edgecolor='black',color='red', alpha=0.3)
-                    ax[i][j].scatter(B[k], B[col], edgecolor='black',color='cyan', alpha=0.3)
-                    i = (i+1)%4 
-                    if i == 0:
-                        j += 1
-            except:
-                print("Please, the 3rd argument must be an integer from 1 to 31")
-                exit(3)
-            fig.delaxes(ax[3,7])
-            plt.show()
+            plot_matrix(M,B)
         else:
             print("Wrong argument was passed.\nPlease, run this with --help for more information")
     except Exception as e:
